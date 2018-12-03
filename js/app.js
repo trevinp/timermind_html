84a8dd8d9130b9020cbac62d18d6776a9ac6ad81
Trevin Pick <trevinp@gmail.com>
2018-12-02 21:43:57 -0800

    Add table columns and editors

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
 //define some sample data
-var tabledata = [
-    {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
-    {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
-    {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
-    {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
-    {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+var eventTableData = [
+    {id:1, ownerid:"123abc", eventname:"Dad Birthday", eventtype:"Birthday", eventdatetime:"01/04/1930", eventoccurencetype:"Yearly"},
+    {id:2, ownerid:"456def", eventname:"Our Anniversary", eventtype:"Anniversary", eventdatetime:"08/16/2013", eventoccurencetype:"Yearly"},
+    {id:4, ownerid:"123jkl", eventname:"Vacation Rental", eventtype:"Misc", eventdatetime:"04/04/2000", eventoccurencetype:"Yearly"},
+    {id:5, ownerid:"456mno", eventname:"Janis Birthday", eventtype:"Birthday", eventdatetime:"12/08/1970", eventoccurencetype:"Yearly"}
 ];
 
-//create Tabulator on DOM element with id "example-table"
-var table = new Tabulator("#example-table", {
+// https://www.jqueryscript.net/table/Easy-Data-Table-Generator-with-jQuery-JSON-Tabulator.html
+// http://tabulator.info/docs/4.0
+
+var table = new Tabulator("#timermind-table", {
     height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-    data:tabledata, //assign data to table
+    data:eventTableData, //assign data to table
     layout:"fitColumns", //fit columns to width of table (optional)
-    columns:[ //Define Table Columns
-        {title:"Name", field:"name", width:150},
-        {title:"Age", field:"age", align:"left", formatter:"progress"},
-        {title:"Favourite Color", field:"col"},
-        {title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
+    resizableColumns: true,
+    columns:[
+        {title:"Event Name", field:"eventname", width:150, editor:"input"},
+        {title:"Type", field:"eventtype", align:"left", editor:"select", editorParams:{values:["Birthday", "Anniversary", "Misc"]}},
+        {title:"Date", field:"eventdatetime", editor:"input"},
+        {title:"Occurence", field:"eventoccurencetype", editor:"select", editorParams:{values:["Yearly", "Monthly", "Weekly", "None"]}},
     ],
     rowClick:function(e, row){ //trigger an alert message when the row is clicked
-        alert("Row " + row.getData().id + " Clicked!!!!");
+        alert("ID " + row.getData().id + " Clicked!");
     },
 });
