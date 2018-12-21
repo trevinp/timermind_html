1cca4f030c46c2756868294bc1499b75f8c45ac6
Trevin <trevinp@gmail.com>
2018-12-20 23:39:14 -0800

    login updates

--- js/app.js
+++ js/app.js
@@ -39,19 +39,4 @@ function SaveNew() {
         eventdatetime:eventDate,eventoccurencetype:eventOccurence}]);
 }
 
-function login() {
-    window.location.href = 'index.html';
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  
-}
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+ 
