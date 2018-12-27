711887459c9c109b39ebfb3b8f24d28edce1684d
Trevin Pick <trevinp@gmail.com>
2018-12-26 16:18:29 -0800

    Refine getevents call

--- js/app.js
+++ js/app.js
@@ -6,29 +6,16 @@ var eventTableData = [
     { id: 5, OwnerId: "456mno", Name: "Janis Birthday", EventType: "Birthday", Date: "12/08/1970", OccurenceType: "Yearly" }
 ];
 
-var eventTableData2 = [];
-getEvents(eventTableData2);
-var table2;
-var data;
+let TIMERMIND_URL = "https://timermind.azurewebsites.net/api/Event/";
+let GET_EVENTS = "GetEventsByOwner/";
+let ownerID = 2;
+let eventTable;
+let data;
+let eventTableData2 = [];
+getEvents(eventTableData2); // call API to retrieve owner events
 
 // https://www.jqueryscript.net/table/Easy-Data-Table-Generator-with-jQuery-JSON-Tabulator.html
 // http://tabulator.info/docs/4.0
-
-/* var table = new Tabulator("#timermind-table", {
-    height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-    data: eventTableData, //assign data to table
-    layout: "fitColumns", //fit columns to width of table (optional)
-    resizableColumns: true,
-    columns: [
-        { title: "Event Name", field: "eventname", width: 150, editor: "input" },
-        { title: "Type", field: "eventtype", align: "left", editor: "select", editorParams: { values: ["Birthday", "Anniversary", "Misc"] } },
-        { title: "Date", field: "eventdatetime", editor: "input" },
-        { title: "Occurence", field: "eventoccurencetype", editor: "select", editorParams: { values: ["Yearly", "Monthly", "Weekly", "None"] } },
-    ],
-    rowClick: function (e, row) { //trigger an alert message when the row is clicked
-        alert("ID " + row.getData().id + " Clicked!");
-    },
-}); */
 
 function SaveNew() {
     var eventName = document.getElementById("description").value;
@@ -39,8 +26,7 @@ function SaveNew() {
     document.getElementById("description").value = "";
     document.getElementById("date").value = "";
     let myEvent = new tmEvent(1, 1, eventName, eventType, 'notes', eventDate, eventOccurence);
-   // eventTableData2.push(myEvent);
-    table2.updateOrAddData([{
+    eventTable.updateOrAddData([{
         Id: 1, OwnerId: "12345", Name: eventName, EventType: eventType,
         Date: eventDate, OccurenceType: eventOccurence
     }]);
@@ -57,14 +43,13 @@ async function saveEvent(newEvent) {
         .then(function () {
             console.log('added new event')
         })
-        .catch (err => console.log('Error in saveevent'));
+        .catch(err => console.log('Error in saveevent'));
 
 }
 async function getAPIStatus() {
     (async () => {
         try {
-            // var response = await fetch('https://timermind.azurewebsites.net/api/Event/GetEventsByOwner/2');
-            var response = await fetch('https://timermind.azurewebsites.net/api/Event/GetAPIStatus');
+            var response = await fetch(TIMERMIND_URL + 'GetAPIStatus');
             var data = await response.json();
             if (data != "OK") alert('API is not working');
             console.log(data);
@@ -74,14 +59,17 @@ async function getAPIStatus() {
     })();
 };
 
-async function getEvents(eventTableData2) {
+
+// Call the API, get events for owner, and refresh the data table
+async function getEvents() {
     (async () => {
         try {
-            let response = await fetch('https://timermind.azurewebsites.net/api/Event/GetEventsByOwner/2');
+            ownerID = document.getElementById("ownerid").value;
+            let response = await fetch(TIMERMIND_URL + GET_EVENTS + ownerID);
             let data = await response.json();
             console.log(data);
             eventTableData2 = data;
-                table2 = new Tabulator("#timermind-table", {
+            eventTable = new Tabulator("#timermind-table", {
                 height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
                 data: eventTableData2, //assign data to table
                 layout: "fitColumns", //fit columns to width of table (optional)
@@ -93,9 +81,9 @@ async function getEvents(eventTableData2) {
                     { title: "Date", field: "Date", editor: "input" },
                     { title: "Occurence", field: "OccurenceType", editor: "select", editorParams: { values: ["Yearly", "Monthly", "Weekly", "None"] } },
                 ],
-                rowClick: function (e, row) { //trigger an alert message when the row is clicked
-                    alert("ID " + row.getData().Id + " Clicked!");
-                },
+                //rowClick: function (e, row) { //trigger an alert message when the row is clicked
+                //    alert("ID " + row.getData().Id + " Clicked!");
+                //},
             });
         } catch (e) {
             console.log("Error in getEvents " + e);
