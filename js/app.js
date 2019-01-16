b258f23a0e0576178f7a201400df56dc27bb6773
Trevin Pick <trevinp@gmail.com>
2019-01-16 02:15:57 -0800

    login logic

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ let GoogleUser;
     window.location.href = "login.html";
 }
  */
+var _ownerID = getCookie('timermindUser');
+document.getElementById("ownerid").value = _ownerID;
 getEvents(eventTableData); // call API to retrieve owner events
 
 // https://www.jqueryscript.net/table/Easy-Data-Table-Generator-with-jQuery-JSON-Tabulator.html
@@ -41,3 +43,24 @@ function login()
     
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
